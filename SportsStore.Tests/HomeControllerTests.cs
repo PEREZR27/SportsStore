5f3cf561a792e8859ecd9eb4979d8aa210db073e
Richard Perez <PEREZR27@my.erau.edu>
2020-09-24 11:42:51 -0500

    Update

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using Xunit;
 namespace SportsStore.Tests
 {
@@ -12,7 +13,21 @@ namespace SportsStore.Tests
         [Fact]
         public void Can_Use_Repository()
         {
-            // ...statements omitted for brevity...
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+ new Product {ProductID = 1, Name = "P1"},
+ new Product {ProductID = 2, Name = "P2"}
+ }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            // Act
+            ProductsListViewModel result =
+            controller.Index().ViewData.Model as ProductsListViewModel;
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
         }
         [Fact]
         public void Can_Paginate()
@@ -29,14 +44,14 @@ namespace SportsStore.Tests
             HomeController controller = new HomeController(mock.Object);
             controller.PageSize = 3;
             // Act
-            IEnumerable<Product> result =
-            (controller.Index(2) as ViewResult).ViewData.Model
-            as IEnumerable<Product>;
-        // Assert
- Product[] prodArray = result.ToArray();
+            ProductsListViewModel result =
+            controller.Index(2).ViewData.Model as ProductsListViewModel;
+         // Assert
+ Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
         }
+...
     }
 }
